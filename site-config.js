6da9c89a1b9f4d5b9ecf6f4e5dc1de213b53594f
iman <imanhamidi91@gmail.com>
2019-02-27 16:26:59 -0800

    add manifest and meta test 1

--- site-config.js
+++ site-config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `Gatsby Universal`,
-  siteTitleShort: `GatsbyU`,
-  siteDescription: `An opinionated starter for Gatsby.`,
-  siteUrl: `https://gu.fabianschultz.com`,
-  themeColor: `#000`,
-  backgroundColor: `#fff`,
+  siteTitle: `Cross Commerce`,
+  siteTitleShort: `Cross Commerce`,
+  siteDescription: `Cross Commerce is a premier Shopify Agency, designing amazing eCommerce experiences`,
+  siteUrl: `https://sleepy-goldberg-4be983.netlify.com/`,
+  themeColor: `#ffffff`,
+  backgroundColor: `#0358aa`,
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
-  social: {
-    twitter: `gatsbyjs`,
-    fbAppId: `966242223397117`,
-  },
+  // social: {
+  //   twitter: `crosscommerce`,
+  //   fbAppId: `966242223397117`,
+  // },
 };
